7833f405a0db74f362d4593a93ee374735b89ec6
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2022-05-03 17:16:35 +0300

    Implementation services added

---
+++ Services/IUserDataService.cs
@@ -0,0 +1,14 @@
+﻿using Library.Contracts.MobileAndLibraryAPI.DTO.Profile;
+using Library.Contracts.MobileAndLibraryAPI.RequestResponse;
+using System.Threading.Tasks;
+
+namespace Library.Services
+{
+    public interface IUserDataService : IService
+    {
+        Task<UserProfile> GetUserProfile(string internalId);
+        Task DeleteProfile(string internalId);
+        Task ChangeUserCommonInfo(string internalId, CommonInfo info);
+        Task ChangeUserDatingCriteria(string internalId, DatingCriteria criteria);
+    }
+}
