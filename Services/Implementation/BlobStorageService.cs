4bd40bec93d2ab6df8321a707bf3d6358bf45cd0
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2022-05-18 10:34:27 +0300

    postman localhost requests

---
+++ Services/Implementation/BlobStorageService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Library.Services.Implementation
+{
+    public class BlobStorageService : IStorageService
+    {
+        public void DelteFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FileExists(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFullUriByName(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveFile(Stream fileStream, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
